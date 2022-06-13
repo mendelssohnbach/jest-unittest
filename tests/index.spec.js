1b1cf39cf267961acfef579f03df1e8d3425d6e3
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-06-13 16:04:47 +0900

    :bug: dir name

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,5 +1,26 @@
-const add = require('../index');
+// 1 を渡すと文字列'1'を返す
+const fizzBuzz = require('../index');
 
-test('状況:1と2を渡す/結果:3を返す', () => {
-  expect(add(1, 2)).toBe(3);
+describe('数値を文字列に変換する', () => {
+  test("1 を渡すと文字列'1'を返す", () => {
+    expect(fizzBuzz(1)).toBe('1');
+  });
+});
+
+describe('3 の倍数を渡すと文字列「Fizz」を返す', () => {
+  test('3 を渡すと文字列「Fizz」を返す', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  });
+});
+
+describe('5 の倍数を渡すと文字列「Buzz」を返す', () => {
+  test('5 を渡すと文字列「Buzz」を返す', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+});
+
+describe('3 と 5 両方の倍数を渡すと文字列「FizzBuzz」を返す', () => {
+  test('15 を渡すと文字列「FizzBuzz」を返す', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
 });
